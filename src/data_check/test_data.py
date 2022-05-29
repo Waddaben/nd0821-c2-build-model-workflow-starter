a9a03dedfcb19221ec9be9e75f750639984b38f5
Waddaben <wadda@iduntechnologies.com>
2022-05-29 14:53:42 +0200

    data splitting sucessfull

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,6 +59,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    """
+    Checks that the size of the dataset is reasonable (not too small, not too large)
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Checks that the size of the dataset is reasonable (not too small, not too large)
+    """
+    assert data['price'].between(min_price, max_price).all()
 
 ########################################################
 # Implement here test_row_count and test_price_range   #
